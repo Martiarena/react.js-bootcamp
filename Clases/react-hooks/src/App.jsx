6abc953fe8bb3db0fd7adaa0e76de8dba9681622
Raúl Martiarena <raulmartiarena89@gmail.com>
2025-03-05 10:57:38 -0500

    clase hoy

--- Clases/react-hooks/src/App.jsx
+++ Clases/react-hooks/src/App.jsx
@@ -1,24 +1,61 @@
-import HookCounterOne from './components/HookCounterOne'
+
 /* 
+import HookCounterOne from './components/HookCounterOne'
 import ClassCounterOne from './components/ClassCounterOne'
 import ClassCounter from './components/ClassCounter'
 import HookCounter from './components/HookCounter'
 import HookCounterFour from './components/HookCounterFour'
 import HookCounterThree from './components/HookCounterThree'
-import HookCounterTwo from './components/HookCounterTwo'*/
+import HookCounterTwo from './components/HookCounterTwo'
+import ClassMouse from "./components/ClassMouse"
+import HookMouse from "./components/HookMouse"
+import IntervalClassCounter from "./components/IntervalClassCounter"
+import IntervalHookCounter from "./components/IntervalHookCounter"
+import MouseContainer from "./components/MouseContainer"
+import DataFetching from "./components/DataFetching"*/
+import React from 'react'
+import ComponentC from "./components/UseContext/ComponentC"
+
+export const UserContext = React.createContext()
+export const ChannelContext = React.createContext()
 
 function App() {
 
   return (
     <>
       <div className='App'>
-        { /*<ClassCounter />
+        { /*
+        ejercicios react
+        - use state
+        - useState with previous state
+        - usestate with object
+        - use state with array
+        - use effect
+        - use effect after redner
+        - conditionally run effect
+        - run effect only once
+        - use effect with clean up
+        - Fetching data with useEffect
+        - Use effect context
+
+        <ClassCounter />
         <HookCounter />
         <HookCounterTwo />
         <HookCounterThree />
         <HookCounterFour />
-        <ClassCounterOne />*/ }
+        <ClassCounterOne />
         <HookCounterOne />
+        <ClassMouse />
+        <MouseContainer />
+        <IntervalClassCounter />
+        <IntervalHookCounter />
+        <DataFetching />
+        */ }
+        <UserContext.Provider value={'Crimsondeath'}>
+          <ChannelContext.Provider value={'TrueRoleDreams'}>
+            <ComponentC />
+          </ChannelContext.Provider>
+        </UserContext.Provider>
       </div>
     </>
   )
